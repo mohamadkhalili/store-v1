5c92ca39ad236ba2d9389bd536de5171b676e7aa
mohamadkhalili <mohamadekhalili100100@gmail.com>
2021-07-23 10:51:12 +0430

    add-static-card-product

---
+++ components/GridCardProduct.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row no-gutters>
+    <v-col
+      v-for="n in 5"
+      :key="n"
+      cols="12"
+      sm="3"
+    >
+      <card-product/>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import CardProduct from "./CardProduct";
+
+export default {
+  name: "GridCardProduct",
+  components: {CardProduct}
+}
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -16,27 +16,7 @@ import Header from "../components/Header";
 export default {
   components: {Header},
   data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+    return {}
   }
 }
 </script>
